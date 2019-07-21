47e8bf222ef7ecce00bc77d53888c52a0fddcec4
joesturge <joe.mike.sturge@gmail.com>
2019-07-21 19:51:03 +0100

    added sockets

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 80;
+const http = require('http');
+const WebSocket = require('ws');
+const url = require('url');
 const Gpio = require('pigpio').Gpio;
+
+const enableWebSockets = app => {
+  app._httpServer = http.createServer(app);
+  app._webSocketServers = [];
+  app.socket = (pathname, wss) => {
+    app._webSocketServers[pathname] = wss;
+  }
+  app.listen = (...args) => {
+    app._httpServer.on('upgrade', function upgrade(request, socket, head) {
+      const pathname = url.parse(request.url).pathname;
+
+      if(pathname in app._webSocketServers) {
+        app._webSocketServers[pathname].handleUpgrade(request, socket, head, function done(ws) {
+          app._webSocketServers[pathname].emit('connection', ws, request);
+        });
+      } else {
+        socket.destroy();
+      }
+    });
+    return app._httpServer.listen(...args);
+  };
+}
+
 const public = process.argv[2];
 
 if (!public) {
   console.log(`ERROR - Please specify a static resource path!`);
   process.exit(1);
 }
+
+const app = express();
+enableWebSockets(app);
+const port = 80;
 
 const ENA = new Gpio(17, {mode: Gpio.OUTPUT});
 const IN1 = new Gpio(27, {mode: Gpio.OUTPUT});
@@ -25,31 +52,33 @@ IN2.digitalWrite(0);
 IN3.digitalWrite(1);
 IN4.digitalWrite(0);
 
-app.use(bodyParser.json());
+const tankControl = new WebSocket.Server({ port: 3001 });
+tankControl.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    if (data.name == "LEFT") {
+      if (data.value < 0 ) {
+        IN1.digitalWrite(0);
+        IN2.digitalWrite(1);
+      } else {
+        IN1.digitalWrite(1);
+        IN2.digitalWrite(0);
+      }
+      ENA.pwmWrite(Math.abs(data.value));
+    } else if (data.name == "RIGHT") {
+      if (data.value < 0) {
+        IN3.digitalWrite(0);
+        IN4.digitalWrite(1);
+      } else {
+        IN3.digitalWrite(1);
+        IN4.digitalWrite(0);
+      }
+      ENB.pwmWrite(Math.abs(data.value));
+    }
+  });
+});
 
 app.use(express.static(path.resolve(public)));
 
-app.post("/control", (request, response) => {
-  if (request.body.name == "LEFT") {
-    if (request.body.value < 0 ) {
-      IN1.digitalWrite(0);
-      IN2.digitalWrite(1);
-    } else {
-      IN1.digitalWrite(1);
-      IN2.digitalWrite(0);
-    }
-    ENA.pwmWrite(Math.abs(request.body.value));
-  } else if (request.body.name == "RIGHT") {
-    if (request.body.value < 0) {
-      IN3.digitalWrite(0);
-      IN4.digitalWrite(1);
-    } else {
-      IN3.digitalWrite(1);
-      IN4.digitalWrite(0);
-    }	  
-    ENB.pwmWrite(Math.abs(request.body.value));
-  }
-  response.status(200).end();
-});
+app.post("/controlSocket", tankControl);
 
 app.listen(port, () => console.log(`Pi Tank Running!`));
